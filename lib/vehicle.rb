256b046f08818d2311d5a59125964d26773a6084
Whitney Muchira <whitneymuchira@gmail.com>
2022-08-29 08:18:57 +0300

    done

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,32 @@
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
+    def initialize(wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+    def go
+        "vrrrrrrrooom!"
+    end
 
-end
+
+    def fill_up_tank
+        "filling up!"
+    end
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#class Vehicle
+#end
